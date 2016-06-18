ecf847d540e8db7df586742aa31dcc82e61bb1b1
ShaneQi <qizengtai@gmail.com>
2016-06-18 00:56:38 -0500

    Created iPad application project.
    Signed-off-by: ShaneQi <qizengtai@gmail.com>

---
+++ Swifty Librarian/Swifty Librarian/BranchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BranchViewController.swift
+//  Swifty Librarian
+//
+//  Created by Shane Qi on 6/17/16.
+//  Copyright © 2016 com.github.shaneqi. All rights reserved.
+//
+
+import UIKit
+
+class BranchViewController: UIViewController {
+
+}
+
---
+++ Swifty Librarian/Swifty Librarian/MasterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MasterViewController.swift
+//  Swifty Librarian
+//
+//  Created by Shane Qi on 6/18/16.
+//  Copyright © 2016 com.github.shaneqi. All rights reserved.
+//
+
+import Foundation
